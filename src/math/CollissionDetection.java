b20f32d952c506fbdd3e2eb08ea5a00aa41deddc
Daniel Langesten <langest@gmail.com>
2013-05-27 19:29:09 +0200

    created acollision detection class.

---
+++ src/math/CollissionDetection.java
@@ -0,0 +1,17 @@
+package math;
+
+public class CollissionDetection {
+	public static final int TYPE_CIRCLE = 0,
+							TYPE_RECT   = 1;
+	
+	public CollissionDetection(int type) {
+		switch (type) {
+		case TYPE_CIRCLE:
+			System.out.println("circle");
+		case TYPE_RECT:
+			System.out.println("rect");
+		default:
+			System.out.println("invalid type");
+		}
+	}
+}
--- src/math/Vector2D.java
+++ src/math/Vector2D.java
@@ -2,8 +2,8 @@ package math;
 
 public class Vector2D {
 	
-	double x;
-	double y;
+	public double x;
+	public double y;
 	
 	public Vector2D(double x, double y) {
 		this.x = x;
@@ -21,6 +21,34 @@ public class Vector2D {
 	
 	public void addToY(double increment) {
 		this.y += increment;
+	}
+	
+	public void add(double x, double y) {
+		this.x += x;
+		this.y += y;
+	}
+	
+	public void addVector(Vector2D vec) {
+		this.x += vec.x;
+		this.y += vec.y;
+	}
+	
+	public void subFromX(double decrement) {
+		this.x -= decrement;
+	}
+	
+	public void subFromY(double decrement) {
+		this.y -= decrement;
+	}
+	
+	public void sub(double x, double y) {
+		this.x -= x;
+		this.y -= y;
+	}
+	
+	public void subVector(Vector2D vec) {
+		this.x -= vec.x;
+		this.y -= vec.y;
 	}
 	
 	public double getMagnitude() {
