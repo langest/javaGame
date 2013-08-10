b193df92e64f9e0874eaaee821e92d4b90c1d475
Daniel Langesten <langest@gmail.com>
2013-08-10 19:59:08 +0200

    worked on collision component rect and maybe some more. ;P

--- src/component/collision/CollisionComponentRect.java
+++ src/component/collision/CollisionComponentRect.java
@@ -13,57 +13,79 @@ import component.ComponentType;
 import entity.Entity;
 
 public class CollisionComponentRect extends Component{
-	
+
 	private float width,
-				  height;
+	height;
 
 	public CollisionComponentRect(Entity owner, float width, float height) {
 		super(ComponentType.COLLISION, owner);
-		
+
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	/**
 	 * Returns the separating axises for this collision component.
+	 * The "o" in the following illustration is the vector for the rect entitys
+	 * x-component SAT if the entity is down to the right and the t
+	 * is the corresponding y-component SAT.
+	 * 
+	 * 	+---------------o-----------> x-axis
+	 * 	|				|
+	 * 	|		
+	 * 	|				|
+	 * 	|
+	 * 	|				|
+	 * 	|
+	 * 	y- - - - - - - -[------]
+	 * 	|				[------]  <-rect entity
+	 * 	|
+	 * 	|
+	 * 	|
+	 * 	V y-axis
+	 * 
+	 * Throws exception if (angle < -Math.PI || Math.PI < angle)
 	 * 
 	 * Angle is the direction to the center of the other collision components owner.
 	 * As the unit circle. As radians.
 	 * First in the list is the owners position, second in list is the vertical SAT, third The horizontal.
 	 */
 	public ArrayList<Vector2D> getSATRect(float angle) {
-		angle = (float) (angle % (Math.PI * 2));
-		ArrayList<Vector2D> ret = new ArrayList<Vector2D>();
+		
+		if (angle < -Math.PI || Math.PI < angle) throw new IllegalArgumentException("angle must be in [-Pi,Pi]");
+		
+		angle = (float) (angle % (Math.PI * 2)); //The angle we want to calculate SAT for
+		ArrayList<Vector2D> ret = new ArrayList<Vector2D>(); //The array that will containing the SATs for given angle
 		Vector2D ownerPos = owner.getPosition();
-		
-		ret.add(owner.getPosition());
-		if (angle < Math.PI/2) {
-			ret.add(new Vector2D(ownerPos.x, ownerPos.y-height/2));
-			ret.add(new Vector2D(width/2, 0));
-		}else if(angle < Math.PI) {
-			ret.add(new Vector2D(0,-height/2));
-			ret.add(new Vector2D(-width/2, 0));
-		}else if (angle < Math.PI*2/3) {
-			ret.add(new Vector2D(0,height/2));
-			ret.add(new Vector2D(-width/2, 0));
-		}else {
-			ret.add(new Vector2D(0,height/2));
-			ret.add(new Vector2D(width/2, 0));
+
+		if (angle < -Math.PI/2) {
+			ret.add(new Vector2D(0,ownerPos.y - height/2));
+			ret.add(new Vector2D(ownerPos.x - width/2, 0));
 		}
-		
+		else if (angle < 0) {
+			ret.add(new Vector2D(0,ownerPos.y - height/2));
+			ret.add(new Vector2D(ownerPos.x + width/2, 0));
+		}
+		else if (angle < Math.PI/2) {
+			ret.add(new Vector2D(0, ownerPos.y + height/2));
+			ret.add(new Vector2D(ownerPos.x - width/2, 0));
+		}
+		else /*if(angle < Math.PI)*/ {
+			ret.add(new Vector2D(0, ownerPos.y + height/2));
+			ret.add(new Vector2D(ownerPos.x + width/2, 0));
+		}
+
 		return ret;
 	}
-	
+
 	@Override
 	public void update(GameContainer gc, BasicGame bg, int delta) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void init() throws ComponentException {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
