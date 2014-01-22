73de2fb7c023031dabfc3666dc4479568ada08a8
Daniel L <daniel.langest@gmail.com>
2014-01-22 18:45:05 +0100

    Making a backup

--- src/component/DownwardsGravityComponent.java
+++
@@ -1,5 +0,0 @@
-package component;
-
-public class DownwardsGravityComponent {
-//TODO
-}
--- src/component/RenderableComponent.java
+++
@@ -1,9 +0,0 @@
-package component;
-
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-
-public interface RenderableComponent {
-        void render(GameContainer gc, BasicGame bg, Graphics gr);
-}
